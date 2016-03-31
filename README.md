f40ddc55dddc7b137d0277865ebd274475eadc05
stassyn <stasinyavskiy@gmail.com>
2016-03-31 10:50:55 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# doppe
+# doppe.js
 Lightweight native javascript drag'n'drop class
+
+## Demo
+
+http://max201.github.io/doppe/
+
+## Installation
+
+Just add script on your page:
+
+    <script type="text/javascript" src="doppe.js"></script>
+
+
+## Usage
+
+	<script type="text/javascript">
+        var d = new Doppe('#draggable', '.handler');
+    </script>
+    <div id="draggable">
+        <header class="handler">Drag</header>
+        <div class="body">
+            <h1>This element is draggable</h1>
+        </div>
+    </div>
---
+++ doppe.js
@@ -0,0 +1,127 @@
+/**
+ * Drag & Drop Native JS Class
+ *
+ * @param element
+ * @param handler
+ * @param callback
+ * @constructor
+ */
+function Doppe(element, handler, callback) {
+    this.element = document.querySelector(element);
+    if ( !this.element ) return;
+    this.handler = this.element.querySelector(handler || element);
+    this.isMoving = false;
+    this.mouse = {x: 0, y: 0};
+    this.mouse.start = {x: 0, y: 0};
+    this.startPosition = this.elementPosition();
+    this.size = {x: this.element.clientWidth, y: this.element.clientHeight};
+    this.continue = null;
+
+    var drg = this;
+
+    // Start drag event handler
+    this.eventStart = function (e) {
+        drg.startPosition = drg.elementPosition();
+        drg.mouse.start = {x: e.x, y: e.y};
+        drg.element.classList.add('moveable');
+        drg.isMoving = true;
+        drg.eventMove();
+    };
+
+    // End drag event handler
+    this.eventStop = function (e) {
+        drg.element.classList.remove('moveable');
+        drg.isMoving = false;
+    };
+
+    // user callback for moving event
+    this.eventMoving = callback || function() {};
+
+    // Mouse move event
+    this.eventMove = function () {
+        drg.render();
+        if ( drg.isMoving ) {
+            requestAnimationFrame(drg.eventMove);
+        }
+    };
+
+    // Events listening
+    this.handler.addEventListener('mousedown', this.eventStart);
+    this.handler.addEventListener('mouseup', this.eventStop);
+
+    this.element.addEventListener('mouseup', this.eventStop);
+
+    // Stop & Continue
+    this.element.addEventListener('mouseover', function(){
+        clearTimeout(drg.continue);
+    });
+    this.element.addEventListener('mouseleave', function() {
+        drg.continue = setTimeout(drg.eventStop, 200);
+    });
+
+    // Mouse position update
+    document.addEventListener('mousemove', function(m) {
+        drg.mouse.x = m.x;
+        drg.mouse.y = m.y;
+    })
+}
+
+Doppe.prototype.render = function () {
+    var offset = this.getOffset();
+    var moveto = {
+        x: this.mouse.x - offset.x,
+        y: this.mouse.y - offset.y
+    };
+
+    this.setPosition(moveto.x, moveto.y);
+    this.eventMoving({
+        element: this.element,
+        handler: this.handler,
+        target: moveto
+    });
+};
+
+Doppe.prototype.setPosition = function (x, y) {
+    var pos = this.filterPosition(x, y);
+    this.element.style.left = pos.x + 'px';
+    this.element.style.top = pos.y + 'px';
+};
+
+Doppe.prototype.getOffset = function () {
+    return {
+        x: this.mouse.start.x - this.startPosition.x,
+        y: this.mouse.start.y - this.startPosition.y
+    };
+};
+
+Doppe.prototype.elementPosition = function () {
+    return {
+        x: this.element.offsetLeft,
+        y: this.element.offsetTop
+    }
+};
+
+Doppe.prototype.filterPosition = function (x, y) {
+    var body = document.getElementsByTagName('body')[0];
+    var mw = body.clientWidth - this.size.x, mh = body.clientHeight - 62;
+    if ( x > mw ) {
+        x = mw;
+    }
+
+    if ( x < 0 ) {
+        x = 0;
+    }
+
+    if ( y > mh ) {
+        y = mh;
+    }
+
+    if ( y < 0 ) {
+        y = 0;
+    }
+
+    return {
+        x: x,
+        y: y
+    }
+};
